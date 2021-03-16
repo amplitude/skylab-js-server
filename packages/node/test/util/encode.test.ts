0f834a5ab90c47e1e60537c9ed29049e51bd739f
Curtis Liu <curtis@amplitude.com>
2021-03-16 12:38:17 -0700

    test: add encode tests

--- packages/node/test/util/encode.test.ts
+++ packages/node/test/util/encode.test.ts
@@ -1,5 +1,18 @@
-import { getUtf8Bytes } from 'src/util/encode';
+import { getUtf8Bytes, urlSafeBase64Encode } from 'src/util/encode';
 
 test('getUtf8Bytes', () => {
-  expect(getUtf8Bytes('a')).toEqual(getUtf8Bytes('a'));
+  expect(getUtf8Bytes('My 🚀 is full of 🦎')).toEqual(
+    new Uint8Array(
+      [
+        77, 121, 32, 240, 159, 154, 128, 32, 105, 115, 32, 102, 117, 108, 108,
+        32, 111, 102, 32, 240, 159, 166, 142
+      ]
+    )
+  );
+});
+
+test('urlSafeBase64Encode', () => {
+  expect(urlSafeBase64Encode('My 🚀 is full of 🦎')).toEqual(
+    "TXkg8J-agCBpcyBmdWxsIG9mIPCfpo4"
+  );
 });
