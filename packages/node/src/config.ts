2cc0b042603e34314ef135c7cce9ed50f93164d2
Brian Giori <brian.giori@amplitude.com>
2021-05-19 20:07:40 -0700

    feat: add timeout and backoff retries

--- packages/node/src/config.ts
+++ packages/node/src/config.ts
@@ -16,6 +16,38 @@ export type SkylabConfig = {
    * The server endpoint from which to request variants.
    */
   serverUrl?: string;
+
+  /**
+   * The request timeout, in milliseconds, used when fetching variants triggered by calling start() or setUser().
+   */
+  fetchTimeoutMillis?: number;
+
+  /**
+   * The number of retries to attempt before failing
+   */
+  fetchRetries?: number;
+
+  /**
+   * Retry backoff minimum (starting backoff delay) in milliseconds. The minimum backoff is scaled by
+   * `fetchRetryBackoffScalar` after each retry failure.
+   */
+  fetchRetryBackoffMinMillis: number;
+
+  /**
+   * Retry backoff maximum in milliseconds. If the scaled backoff is greater than the max, the max is
+   * used for all subsequent retries.
+   */
+  fetchRetryBackoffMaxMillis: number;
+
+  /**
+   * Scales the minimum backoff exponentially.
+   */
+  fetchRetryBackoffScalar: number;
+
+  /**
+   * The request timeout for retrying fetch requests.
+   */
+  fetchRetryTimeoutMillis?: number;
 };
 
 /**
@@ -26,6 +58,13 @@ export type SkylabConfig = {
  | **debug**        | false                           |
  | **instanceName** | `"$default_instance"`             |
  | **serverUrl**    | `"https://api.lab.amplitude.com"` |
+ | **fetchTimeoutMillis**    | `10000` |
+ | **fetchRetries**    | `5` |
+ | **fetchRetryBackoffMinMillis**    | `500` |
+ | **fetchRetryBackoffMaxMillis**    | `10000` |
+ | **fetchRetryBackoffScalar**    | `2` |
+ | **fetchRetryTimeoutMillis**    | `10000` |
+
 
  *
  * @category Configuration
@@ -34,4 +73,10 @@ export const Defaults: SkylabConfig = {
   debug: false,
   instanceName: '$default_instance',
   serverUrl: 'https://api.lab.amplitude.com',
+  fetchTimeoutMillis: 10000,
+  fetchRetries: 8,
+  fetchRetryBackoffMinMillis: 500,
+  fetchRetryBackoffMaxMillis: 10000,
+  fetchRetryBackoffScalar: 1.5,
+  fetchRetryTimeoutMillis: 10000,
 };
